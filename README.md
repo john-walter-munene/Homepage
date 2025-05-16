4ba1d70ebae4bf8e4b41b6ca4b17dc12c964933e
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2025-05-17 00:08:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Development Template
+
+This repository contains basic Webpack configurations for which I can start building my web projects on.
+
+It also contains the Jest testing framework for my Test Driven development. 
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        // Main files.
+        index: './src/index.js',
+    },
+    devtool: "eval-source-map",
+    devServer: {
+      watchFiles: ["./src/index.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            scriptLoading: 'defer'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: 'src/404.html', to: '404.html' }
+            ]
+          }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test:  /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+  };
